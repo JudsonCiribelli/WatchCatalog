fea7a30739bf1399dd85776cdb35a216c0df3258
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-24 18:26:58 -0300

    feat: Listing our series on the Home Page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,7 @@
-import Api from "../../services/api.js";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+//Services
+import Api from "../../services/api.js";
 const Home = () => {
   const [series, setSeries] = useState([]);
 
@@ -12,14 +14,24 @@ const Home = () => {
           page: 1,
         },
       });
-      console.log(response);
+      setSeries(response.data.slice(0, 20));
     }
     LoadSeries();
   }, []);
 
   return (
-    <div>
-      <h1>Pagina Home</h1>
+    <div className="ml-16">
+      <div className="grid grid-cols-4 gap-4  max-w-full  my-5  ml-16">
+        {series.map((series) => {
+          return (
+            <article key={series.id} className="w-full p-3.5 rounded-md">
+              <h1 className=" block mb-4 text-base">{series.name}</h1>
+              <img src={series.image.medium} alt={series.name} />
+              <Link to={`/details/${series.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 };
