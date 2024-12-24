50ed997699f917d1d21f3103ac65a83ed64c3a89
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-24 18:38:10 -0300

    feat: add Button Component

---
+++ src/Components/Custom-Button/Button-Component.js
@@ -0,0 +1,8 @@
+const CustomButton = () => {
+  return (
+    <button className="bg-black w-6/12 flex justify-center align-center rounded-xl border-none  text-white py-2.5 px-3.5 font-bold">
+      Acessar
+    </button>
+  );
+};
+export default CustomButton;
