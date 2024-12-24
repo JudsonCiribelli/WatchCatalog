de646c1d27a370f332b6fc74ca7b955146b7cdcd
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-24 19:12:44 -0300

    styled: add styles to Button Component

--- src/Components/Custom-Button/Button-Component.js
+++ src/Components/Custom-Button/Button-Component.js
@@ -1,6 +1,6 @@
 const CustomButton = () => {
   return (
-    <button className="bg-black w-6/12 flex justify-center align-center rounded-xl border-none  text-white py-2.5 px-3.5 font-bold">
+    <button className="bg-black w-6/12 mt-4 flex justify-center align-center rounded-xl border-none  text-white py-2.5 px-3.5 font-bold">
       Acessar
     </button>
   );
