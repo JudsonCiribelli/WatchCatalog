4882b8de601438aafc03241e67c653b071048ef0
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-27 11:24:40 -0300

    feat: add responsiveness to Button Component

--- src/Components/Custom-Button/Button-Component.js
+++ src/Components/Custom-Button/Button-Component.js
@@ -1,6 +1,6 @@
 const CustomButton = ({ children }) => {
   return (
-    <button className="bg-black w-full mt-4 flex justify-center align-center rounded-xl border-none  text-white py-2.5 px-3.5 font-bold">
+    <button className="bg-black w-full mt-4 flex justify-center align-center rounded-xl border-none  text-white py-2.5 px-3.5 font-bold sm:text-4xl sm:p-6">
       {children}
     </button>
   );
--- src/Components/Header/Header-Component.js
+++ src/Components/Header/Header-Component.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="flex  items-center justify-around w-full h-16 bg-[#2D3142] px-4 sm:px-8 md:px-12">
+    <header className="flex  items-center justify-around w-full sm:h-36 bg-[#2D3142] px-4 sm:px-8 md:px-12">
       <Link
         className="no-underline text-3xl text-white font-bold cursor-pointer  "
         to="/"
@@ -10,7 +10,7 @@ const Header = () => {
         WatchCatalog
       </Link>
       <Link
-        className="cursor-pointer  bg-white py-2 px-4  sm:px-6  no-underline text-black rounded text-sm sm:text-base md:text-sm md:text-lg"
+        className="cursor-pointer  bg-white py-2 px-4  sm:px-6  no-underline text-black rounded text-sm sm:text-3xl md:text-sm md:text-lg"
         to="/favorites"
       >
         Meus Favoritos
