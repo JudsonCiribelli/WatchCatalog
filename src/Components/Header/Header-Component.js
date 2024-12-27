d9382cfdf1d51af4fabf2acf110836ae5830f109
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-27 11:15:51 -0300

    feat: add responsiveness to Header Component

--- src/Components/Header/Header-Component.js
+++ src/Components/Header/Header-Component.js
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="flex  items-center justify-around w-full h-16 bg-[#2D3142]">
+    <header className="flex  items-center justify-around w-full h-16 bg-[#2D3142] px-4 sm:px-8 md:px-12">
       <Link
-        className="no-underline text-3xl text-white font-bold cursor-pointer "
+        className="no-underline text-3xl text-white font-bold cursor-pointer  "
         to="/"
       >
         WatchCatalog
       </Link>
       <Link
-        className="cursor-pointer bg-white py-3 px-1.5 no-underline text-black rounded"
+        className="cursor-pointer  bg-white py-2 px-4  sm:px-6  no-underline text-black rounded text-sm sm:text-base md:text-sm md:text-lg"
         to="/favorites"
       >
         Meus Favoritos
